37c697f041380bebf6114b5b1fe1d6509a4d8dda
megan levy <69728544+megan-levy@users.noreply.github.com>
2021-05-30 19:50:15 -0400

    Synonyms added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
@@ -8,17 +9,18 @@ export default function Meaning(props) {
             {props.meaning.partOfSpeech}
             </small>
         </h3>
-        {props.meaning.definitions.map(function(definition, index){
+        {props.meaning.definitions.map(function(definition, index) {
             return (
                 <div key = {index}>
-                <p>
-                 - {definition.definition}
-                <br/>
-                 <em>{definition.example}</em>
-                </p>
+                    <p>
+                    {definition.definition}
+                    <br/>
+                    <em>{definition.example}</em>
+                    <Synonyms synonyms={definition.synonyms} />
+                    </p>
                 </div>
             );
         })}
         </div>
-    )
+    );
 }
